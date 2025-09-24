c5c0a188b3fe8d0ebb2a3cc819dbe188072a0948
ahmetkorkmaz27 <76773742+ahmetkorkmaz27@users.noreply.github.com>
2025-09-24 14:59:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import json
 from datetime import datetime
+import requests
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+def binance_access_test():
+    try:
+        r = requests.get("https://api.binance.com/api/v3/time", timeout=5)
+        if "serverTime" in r.json():
+            return "✅ Binance API erişimi açık."
+        else:
+            return "❌ Binance API engelli."
+    except Exception as e:
+        return f"⚠️ Hata oluştu: {str(e)}"
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
@@ -16,10 +27,13 @@ async def read_root(request: Request):
         data = []
 
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    binance_status = binance_access_test()
+
     return templates.TemplateResponse("index.html", {
         "request": request,
         "data": data,
-        "timestamp": timestamp
+        "timestamp": timestamp,
+        "binance_status": binance_status
     })
 
 
